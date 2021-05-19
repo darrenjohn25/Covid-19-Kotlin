88714b8f3c6207d26e058c76020b3b05a57c400b
vimalrajan242001 <vimalrajansret@gmail.com>
2021-05-19 15:27:50 +0530

    app crash fixed india stats added

--- app/src/main/java/com/nopalyer/covidapp/KnowMoreIndiaActivity.kt
+++ app/src/main/java/com/nopalyer/covidapp/KnowMoreIndiaActivity.kt
@@ -1,7 +1,5 @@
 package com.nopalyer.covidapp
 
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +10,7 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_know_more.*
 import kotlinx.android.synthetic.main.activity_know_more_india.*
+
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
@@ -20,65 +19,35 @@ class KnowMoreIndiaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_know_more_india)
-      getGlobaltodayData()
-      getGlobalData()
+      getGlobalIndiaData()
     }
-    fun getGlobaltodayData(){
+    fun getGlobalIndiaData(){
     val url:String ="https://covid19.mathdro.id/api/countries/INDIA"
 
     val stringRequest = StringRequest(
         Request.Method.GET,
         url,
-        Response.Listener<String> {
+        {
 
             var jsonObject = JSONObject(it.toString())
             var confirmed = JSONObject(jsonObject.getString("confirmed").toString())
             var recovered = JSONObject(jsonObject.getString("recovered").toString())
             var deaths = JSONObject(jsonObject.getString("deaths").toString())
-            Log.i("KnowMoreIndiaActivity","today - "+confirmed.getString("value"))
+            Log.i("KnowMoreIndiaActivity","inida confirmed - "+confirmed.getString("value"))
             txtIndiaTodayInfected.text = confirmed.getString("value")
             txtIndiaTodayRecoverd.text = recovered.getString("value")
             txtIndiaTodayDeceased.text = deaths.getString("value")
-        },
-        Response.ErrorListener {
+                                  },
+        {
             Toast.makeText(this@KnowMoreIndiaActivity,it.toString(),Toast.LENGTH_LONG).show()
             txtIndiaTodayInfected.text = "-"
             txtIndiaTodayRecoverd.text = "-"
             txtIndiaTodayDeceased.text = "-"
-
         }
     )
 
     val requestQueue = Volley.newRequestQueue(this@KnowMoreIndiaActivity)
     requestQueue.add(stringRequest)
 }
-
-    fun getGlobalData(){
-        val url:String ="https://covid19.mathdro.id/api/countries/INDIA"
-
-        val stringRequest = StringRequest(
-            Request.Method.GET,
-            url,
-            Response.Listener<String> {
-                var jsonObject = JSONObject(it.toString())
-                var confirmed = JSONObject(jsonObject.getString("confirmed").toString())
-                var recovered = JSONObject(jsonObject.getString("recovered").toString())
-                var deaths = JSONObject(jsonObject.getString("deaths").toString())
-                //now set values in textview
-                txtIndiaTotalInfected.text = confirmed.getString("value")
-                txtIndiaTotalRecoverd.text = recovered.getString("value")
-                txtIndiaTotalDeceased.text = deaths.getString("value")
-            },
-            Response.ErrorListener {
-                Toast.makeText(this@KnowMoreIndiaActivity,it.toString(),Toast.LENGTH_LONG).show()
-                txtIndiaTotalInfected.text = "-"
-                txtIndiaTotalRecoverd.text = "-"
-                txtIndiaTotalDeceased.text = "-"
-            }
-        )
-
-        val requestQueue = Volley.newRequestQueue(this@KnowMoreIndiaActivity)
-        requestQueue.add(stringRequest)
-    }
 
 }
